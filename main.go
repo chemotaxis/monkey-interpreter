8f57ef5f8c083e893ab9e6ea00b2674fe49ddf22
chemotaxis <ensminger.ethan@gmail.com>
2020-06-05 02:15:48 -0400

    Added a main file
    Grabs the user's name and starts repl.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hi %s!  This is the Monkey language REPL.\n", user.Username)
+	fmt.Printf("Feel free to type in commands.\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
