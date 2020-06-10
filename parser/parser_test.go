ee45aa352d4cfc8413985f2e5857d64894821677
chemotaxis <ensminger.ethan@gmail.com>
2020-06-10 16:33:05 -0400

    Added test case to check bad source code input

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -43,6 +43,45 @@ let foobar = 838383;
 	}
 }
 
+// TestBadLetStatements tests what happens with bad source code
+func TestBadLetStatements(t *testing.T) {
+	input := `
+let x 5;
+let = 10;
+let 838383;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+// testLetStatement is a helper function to test fields in the LetStatement type
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
