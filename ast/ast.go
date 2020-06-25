d1b3a041f176762e021fbd4c8d3b332fb9ef081b
chemotaxis <ensminger.ethan@gmail.com>
2020-06-25 12:55:15 -0400

    Add comments

--- ast/ast.go
+++ ast/ast.go
@@ -6,24 +6,31 @@ package ast
 
 import "monkey/token"
 
+// Node represents a node in the abstract syntax tree.
 type Node interface {
 	TokenLiteral() string
 }
 
+// Statement represents a statement.  Currently, there are only two types of
+// statements: let (for assignment) and return (for returning expressions)
+// statements.
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// Expression represents an expression that is to be evaluated to a value.
 type Expression interface {
 	Node
 	expressionNode()
 }
 
+// Program represents the whole syntax tree for a program.
 type Program struct {
 	Statements []Statement
 }
 
+// TokenLiteral returns the literal for the root node in the program.
 func (p *Program) TokenLiteral() string {
 	var literal string
 	if len(p.Statements) > 0 {
@@ -33,6 +40,8 @@ func (p *Program) TokenLiteral() string {
 	return literal
 }
 
+// LetStatement represents the statement used to assign values to identifiers
+// (ie variables).  E.g. let x = 34;
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -40,6 +49,8 @@ type LetStatement struct {
 }
 
 func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral returns the text character used to represent the let keyword.
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
@@ -66,7 +77,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 
-// TokenLiteral returns the text character used to for this identifier.
+// TokenLiteral returns the text character used for this identifier.
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
