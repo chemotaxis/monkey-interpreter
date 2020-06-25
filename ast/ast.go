50b3cb3e268f213e3ca71d257f2aba948e26255f
chemotaxis <ensminger.ethan@gmail.com>
2020-06-25 11:57:48 -0400

    Add representation for return statement

--- ast/ast.go
+++ ast/ast.go
@@ -44,6 +44,20 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+// ReturnStatement represents a return statement (e.g. return <expression>).
+type ReturnStatement struct {
+	Token       token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral returns the actual text character used to represent the return
+// token.
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
