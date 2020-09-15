d64af3d51c62c551ee6f6cef99c3cc08c49af3ea
chemotaxis <chemotaxis@users.noreply.github.com>
2020-09-14 21:40:48 -0400

    Define FunctionLiteral
    This defines our functions as expressions by fulfilling the expression
    node interface.

--- ast/ast.go
+++ ast/ast.go
@@ -7,6 +7,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 // Node represents a node in the abstract syntax tree.
@@ -302,6 +303,37 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+// FunctionLiteral represents a function expression.
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+// TokenLiteral returns a string representation of the FunctionLiteral token.
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
