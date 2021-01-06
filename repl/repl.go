116e92be237173616c09fe61c5077554c87cb73b
chemotaxis <chemotaxis@users.noreply.github.com>
2021-01-06 18:56:45 -0500

    Add parsing to interactive shell

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
 )
 
 // PROMPT is printed at the beginning of every line
@@ -26,19 +26,27 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
 
-		if line == "exit" {
-			return
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
 
-		lex := lexer.New(line)
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 
-		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
 		if line == "exit" {
 			fmt.Println()
 			return
 		}
+	}
+}
 
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
