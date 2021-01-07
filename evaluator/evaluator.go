af20330800474e0e82f910597fb02f1837574a2d
chemotaxis <chemotaxis@users.noreply.github.com>
2021-01-07 00:52:36 -0500

    Traverse ast
    Recursively evaluate statement and expression.
    
    Test output:
    
    $go test ./evaluator/
    ok      monkey/evaluator        0.011s

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,12 +6,32 @@ import (
 	"monkey/object"
 )
 
-// Eval evaluates a node and returns the node's value
+// Eval evaluates a node and returns the node's value or traverses to the next
+// expression to be evaluated.
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 
 	return nil
+}
+
+// evalStatements recursively evaluates each statement.  It returns the value of
+// the last statement in the program.
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
 }
