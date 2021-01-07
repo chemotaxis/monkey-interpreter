ae43b7a25d419b912c4b9a806a4a6789d548a5bd
chemotaxis <chemotaxis@users.noreply.github.com>
2021-01-06 20:42:26 -0500

    Add Integer object

--- object/object.go
+++ object/object.go
@@ -2,6 +2,13 @@
 // the Monkey language.
 package object
 
+import "fmt"
+
+// List of different objects supported in Monkey.
+const (
+	INTEGER_OBJ = "INTEGER"
+)
+
 // ObjectType represents a value.  All types are represented as Objects.
 type ObjectType string
 
@@ -9,4 +16,19 @@ type ObjectType string
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+// Integer represents all integers.
+type Integer struct {
+	Value int64
+}
+
+// Type returns the type of Integer.
+func (i *Integer) Type() ObjectType {
+	return INTEGER_OBJ
+}
+
+// Inspect returns a string representation of the Integer type.
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
 }
