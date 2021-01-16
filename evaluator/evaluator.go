62fa9bfa6c2602e35ef33ea362e98fdd29247c07
chemotaxis <chemotaxis@users.noreply.github.com>
2021-01-16 01:13:07 -0500

    Evaluate bang (!) operator expressions
    Tests pass:
    
    $go test ./evaluator/
    ok      monkey/evaluator        0.011s

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -33,6 +33,10 @@ func Eval(node ast.Node) object.Object {
 			return TRUE
 		}
 		return FALSE
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -48,4 +52,29 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+// evalPrefixExpression evaluates bang (!) and minus (-) operators.  Currently,
+// it only supports (!).
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// evalBangOperatorExpression defines the behavior of the bang (!) operator.
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
