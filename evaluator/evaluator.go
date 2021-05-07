98c2769ea195c51ff559dce1193e4f8b03e2adc7
chemotaxis <chemotaxis@users.noreply.github.com>
2021-05-07 17:45:46 -0400

    Evaluate if expressions
    Tests pass:
    
    $go test ./evaluator
    ok      monkey/evaluator        (cached)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -39,9 +39,43 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
+	// This evaluates the block statements in each branch of the if expression.
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
 	}
 
 	return nil
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+// isTruthy defines what objects are "truthy".  Basically, it's any object that
+// is _not_ NULL or false.
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 // nativeBoolToBooleanObject switches type to boolean object
